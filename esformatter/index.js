a31d18b8afd85a3a2db67a9daec64c762750aa6c
futurist <jamesyang999@gmail.com>
2016-12-10 22:35:39 +0800

    add: esformatter style

---
+++ esformatter/index.js
@@ -0,0 +1,19 @@
+const {format} = require('esformatter')
+const style = require('./style.json')
+
+/**
+ * format js code, then response back to user
+ * @param {string} code the string of code to format
+ * @param {function} cb callback to call when format finished: (err, out)->any
+ * @returns {any}
+ */
+function formatter(code, cb) {
+	try {
+		let result = format(code, style)
+		cb(null, result)
+	} catch(e) {
+		cb(e)
+	}
+}
+
+module.exports = formatter
